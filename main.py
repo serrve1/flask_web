0387b3e1869ae68b29625478257fa5f9b045c091
serrve1 <gustjqdl5056@gmail.com>
2024-01-31 13:17:44 +0900

    index.html

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+# 특정 가상환경에서만 사용 가능한 폴더들... 깃허브에 올리면 안되는 것을 ignore에 정리 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#@app.route('/')은 기본인 get방식으로 요청
+#@app.route('/',methods= ['post'])처럼 방식을 지정할 수도 있습니다. 
 @app.route('/')
 def index():
-    return "Hello World!!"
+    return render_template('index.html')
+@app.route('/hello')
+# 함수명을 index와 다르게 주어야한다.
+def hello():
+    return "안녕 반가워"
+
 
 if __name__ == '__main__':
     app.run(debug = True) # flask에서는 이거만하면됨 (패스트api에서는 cmd: reload ) 
